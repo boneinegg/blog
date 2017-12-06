abc7fb5b78a9667e30ee0c17dba987e31bf9730f
boneinegg <316786195@qq.com>
2017-12-06 12:02:09 +0800

    auth

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import Form
 from wtforms import StringField, BooleanField, SubmitField, PasswordField
 from wtforms.validators import Email, DataRequired, Length
 
-class loginForm(Form):
+class LoginForm(Form):
     email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
